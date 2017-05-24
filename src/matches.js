075d12f7823400f348c6840feba9343519b74074
Emanuel Tannert <post@etannert.de>
2017-05-24 13:41:45 +0200

    check in all the things

---
+++ src/matches.js
@@ -0,0 +1,88 @@
+/**
+ * A matches function that climbs up the document tree,
+ * kind of like what gator.js does.
+ *
+ * Author: Emanuel Tannert <post@etannert.de>
+ * Repository: https://github.com/sub-lunar/match
+ *
+ * This is useful for event delegation. Example:
+ * You have a list with many items and want to
+ * listen for clicks on a list item. Instead of
+ * attaching an eventListener on each and every
+ * list item, you just attach one eventListener
+ * on the whole list, which is more efficient,
+ * because click events bubble up. Now you want
+ * to detect wether a click was on a list item
+ * or just on whitespace within the list. The
+ * problem is that your list items have nested
+ * child elements, and if the user clicks on one
+ * of them, the browser gives you that nested
+ * child as the event.target instead of the list
+ * item. So you need a way to find out wether
+ * your event.target or any of its parent elements
+ * match a certain selector. This is what this
+ * bubbling match function is for.
+ *
+ * Maybe you know gator.js; it does exactly that.
+ * But it also registers the eventListener for you
+ * and it sneaks in the matched element as this
+ * into your callback function instead of using
+ * a regular argument, which is kind of weird and
+ * you can't for example use an ES6 arrow function
+ * as a callback if you need the element.
+ * This module is much more basic than gator.js.
+ * It just exports a match function that you call
+ * with the event.target (or any other element)
+ * and a selector string and it returns you the
+ * matched element, if any, or null.
+ * Note that it doesn't actually implement a matches
+ * method. If it doesn't find one in the browser,
+ * it throws an error, so you may want to use an
+ * element.matches polyfill if you are targeting
+ * legacy browsers.
+ *
+ * Code Example:
+ *
+ * import matches from '@sub-lunar/matches'
+ *
+ * const list = document.querySelector('.my-list')
+ *
+ * list.addEventListener('click', event => {
+ *
+ *   if (matches(event.target, '.my-list__item')) {
+ *     console.log('click inside list item')
+ *   } else {
+ *     console.log('click on whitespace')
+ *   }
+ * })
+ */
+
+const matchMethod = getMatchMethod()
+
+module.exports = function matches (element, selector) {
+  while (element.tagName !== 'HTML' && element.tagName !== 'html') {
+    if (element[matchMethod](selector)) {
+      return element
+    }
+    element = element.parentNode
+  }
+  return null
+}
+
+function getMatchMethod () {
+  const body = document.body
+  const methods = [
+    'matches',
+    'matchesSelector',
+    'mozMatchesSelector',
+    'webkitMatchesSelector',
+    'msMatchesSelector',
+  ]
+  for (let i = 0; i < methods.length; i++) {
+    if (Object.prototype.toString.call(body[methods[i]])
+        === '[object Function]') {
+      return methods[i]
+    }
+  }
+  throw new Error('No matches or similiar method found on document.body')
+}
---
+++ test/mock.js
@@ -0,0 +1,203 @@
+import test from 'ava'
+import jsdom from 'jsdom'
+import SandboxedModule from 'sandboxed-module'
+
+// mock up before import
+const dom = new jsdom.JSDOM(`
+
+<!DOCTYPE html>
+<html>
+  <body>
+
+    <!-- A List like in the README Example -->
+
+    <ul class="my-list">
+      <li class="my-list__item">
+        <img class="my-list__image" src="" alt="" />
+        <a class="my-list__link" href="/">Click me</a>
+      </li>
+      <li class="my-list__item">
+        <img class="my-list__image" src="" alt="" />
+        <a class="my-list__link" href="/">Click me</a>
+      </li>
+      <li class="my-list__item">
+        <img class="my-list__image" src="" alt="" />
+        <a class="my-list__link" href="/">Click me</a>
+      </li>
+    </ul>
+
+    <!-- An example with nested divs to show a non-match -->
+
+    <div class="wrapper">
+      <div class="want-to-match">
+        <div class="click-me">
+        </div>
+      </div>
+      <div class="match-me-not">
+        <div class="click-me">
+        </div>
+      </div>
+    </div>
+
+  </body>
+</html>
+`)
+
+const document = dom.window.document
+
+// also a function to trigger events, copied from StackOverflow
+// https://stackoverflow.com/a/2706236/3254410
+function eventFire(el, etype){
+  if (el.fireEvent) {
+    el.fireEvent('on' + etype)
+  } else {
+    var evObj = document.createEvent('Events')
+    evObj.initEvent(etype, true, false)
+    el.dispatchEvent(evObj)
+  }
+}
+
+const matches = SandboxedModule.require('../dist/matches', {
+  globals: { document },
+})
+
+
+test('matches if target is child', t => {
+
+  t.plan(9) // from outer space
+
+  const myList = document.querySelector('.my-list')
+
+  let numberMatches = 0
+  let lastMatch = null
+
+  myList.addEventListener('click', event => {
+    const match = matches(event.target, '.my-list__item')
+    if (match) {
+      numberMatches++
+      lastMatch = match
+    }
+  })
+
+  const links = document.querySelectorAll('.my-list__link')
+
+  // second link first, just for fun
+  eventFire(links[1], 'click')
+
+  t.is(numberMatches, 1)
+  t.is(lastMatch.className, 'my-list__item')
+  t.is(lastMatch.tagName, 'LI')
+
+  lastMatch = null
+
+  // then first one
+  eventFire(links[0], 'click')
+
+  t.is(numberMatches, 2)
+  t.is(lastMatch.className, 'my-list__item')
+  t.is(lastMatch.tagName, 'LI')
+
+  lastMatch = null
+
+  // then last one
+  eventFire(links[2], 'click')
+
+  t.is(numberMatches, 3)
+  t.is(lastMatch.className, 'my-list__item')
+  t.is(lastMatch.tagName, 'LI')
+})
+
+
+test('matches if target matches selector directly', t => {
+
+  t.plan(9)
+
+  const myList = document.querySelector('.my-list')
+
+  let numberMatches = 0
+  let lastMatch = null
+
+  myList.addEventListener('click', event => {
+    const match = matches(event.target, '.my-list__item')
+    if (match) {
+      numberMatches++
+      lastMatch = match
+    }
+  })
+
+  const items = document.querySelectorAll('.my-list__item')
+
+  eventFire(items[2], 'click')
+
+  t.is(numberMatches, 1)
+  t.is(lastMatch.className, 'my-list__item')
+  t.is(lastMatch.tagName, 'LI')
+
+  lastMatch = null
+
+  eventFire(items[1], 'click')
+
+  t.is(numberMatches, 2)
+  t.is(lastMatch.className, 'my-list__item')
+  t.is(lastMatch.tagName, 'LI')
+
+  lastMatch = null
+
+  eventFire(items[0], 'click')
+
+  t.is(numberMatches, 3)
+  t.is(lastMatch.className, 'my-list__item')
+  t.is(lastMatch.tagName, 'LI')
+})
+
+test('matches and non-matches', t => {
+
+  t.plan(10)
+
+  const wrapper = document.querySelector('.wrapper')
+  const wantToMatch = document.querySelector('.want-to-match')
+  const matchMeNot = document.querySelector('.match-me-not')
+  const clickMes = document.querySelectorAll('.click-me')
+
+  let numberMatches = 0
+  let lastMatch = null
+
+  wrapper.addEventListener('click', ({ target }) => {
+
+    const match = matches(target, 'div.want-to-match')
+
+    if (match) {
+      numberMatches++
+      lastMatch = match
+    }
+  })
+
+  eventFire(clickMes[0], 'click')
+
+  t.is(numberMatches, 1)
+  t.is(lastMatch, wantToMatch)
+
+  lastMatch = null
+
+  eventFire(clickMes[1], 'click')
+
+  t.is(numberMatches, 1)
+  t.is(lastMatch, null)
+
+  eventFire(wantToMatch, 'click')
+
+  t.is(numberMatches, 2)
+  t.is(lastMatch, wantToMatch)
+
+  lastMatch = null
+
+  eventFire(matchMeNot, 'click')
+
+  t.is(numberMatches, 2)
+  t.is(lastMatch, null)
+
+  eventFire(wrapper, 'click')
+
+  t.is(numberMatches, 2)
+  t.is(lastMatch, null)
+})
